f79ba63f0cb7f500bec2f1ff93ff91e7b6ea4b07
herve.teguia <herve.teguia@kps-it.com>
2020-03-21 12:13:59 +0100

    up

---
+++ ConsoleJenkins/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleJenkins
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write(CreateMessage());
+        }
+
+        public static string  CreateMessage()
+        {
+            return "Hello World";
+        }
+    }
+}
---
+++ ConsoleJenkinsTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("ConsoleJenkinsTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ConsoleJenkinsTest")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("5f42af91-9a6c-48fc-aa6d-5b532bfd0ef2")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
